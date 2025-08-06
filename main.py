bae968695ac78ffcbcfa966b42d21adda9dbac28
Bhaveshkr1 <113119001+Bhaveshkr1@users.noreply.github.com>
2025-08-06 12:55:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from utils import summarize_text
+
+if __name__ == "__main__":
+    text = "Artificial Intelligence is transforming research."
+    summary = summarize_text(text)
+    print("Summary:", summary)
